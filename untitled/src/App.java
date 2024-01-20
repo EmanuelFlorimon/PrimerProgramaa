870a8d6414795e3cd122f850a3697fc466021099
EmanuelFlorimon <156150510+EmanuelFlorimon@users.noreply.github.com>
2024-01-19 21:47:55 -0400

    repositorio

---
+++ untitled/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) {
+
+        String nombre = "Emanuel";
+
+        double monto = 475.50;
+
+        double resultado = monto * 0.15;
+
+        System.out.println( "bienvenido " + nombre );
+
+        System.out.println( "el resultado es " + resultado);
+    }
+
+
+}
